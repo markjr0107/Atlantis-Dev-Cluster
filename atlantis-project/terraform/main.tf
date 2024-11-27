358a1091dd660d8070683b765a58d27da7abf524
Mark Junior <81783075+markjr0107@users.noreply.github.com>
2024-11-27 16:53:27 -0300

    Update main.tf

--- atlantis-project/terraform/main.tf
+++ atlantis-project/terraform/main.tf
@@ -2,6 +2,28 @@ provider "aws" {
   region = var.aws_region
 }
 
+# IAM Role para o cluster
+resource "aws_iam_role" "eks_role" {
+  name = "${var.cluster_name}-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+# Cluster EKS
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_role.arn
@@ -9,8 +31,11 @@ resource "aws_eks_cluster" "this" {
   vpc_config {
     subnet_ids = var.subnet_ids
   }
-}
 
-output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  depends_on = [aws_iam_role.eks_role]
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
 }
